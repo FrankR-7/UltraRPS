a77dd487f3ae95c56b1aae71a1f2d314e10d4a50
FrankR-7 <franklinrosales7@gmail.com>
2020-09-11 22:01:16 -0600

    Added buttons

--- classes.py
+++ classes.py
@@ -3,7 +3,29 @@ import pygame as pg
 class Hand:
     def __init__(self, img):
         self.img = img
-        self.disp = 250 - img.width # displacement
-        self.y = 50
+        self.y = 400 - self.img.get_height()
         self.dec = None
         self.prevs = []
+
+    def draw(self, win, bot=False):
+        x = 75 if not bot else 625
+        win.blit(self.img, (x, self.y))
+
+class Button:
+    def __init__(self, img, pos):
+        self.img = pg.transform.scale(img, (100, 100))
+        self.pos = pos
+        self.y = 450
+        if pos == 1:
+            self.x = 175
+        elif pos == 2:
+            self.x = 400
+        elif pos == 3:
+            self.x = 625
+
+    def draw(self, win):
+        win.blit(self.img, (self.x, self.y))
+
+    def is_pressed(self):
+        mouse = pg.mouse.get_pos()
+        return self.x < mouse[0] < self.x+100 and self.y < mouse[1] < self.y+100
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame as pg
 import os
+from classes import Hand, Button
 
 ROCK = pg.transform.scale(pg.image.load(os.path.join('Assets','rock.webp')), (230,180))
 PAPER = pg.transform.scale(pg.image.load(os.path.join('Assets','paper.webp')), (200,250))
@@ -11,21 +12,68 @@ LOSE = pg.transform.scale(pg.image.load(os.path.join('Assets','defeat.webp')), (
 WIDTH, HEIGHT = 900, 600
 WIN = pg.display.set_mode((WIDTH, HEIGHT))
 pg.display.set_caption('Ultra RPS')
-pg.display.set_icon(SCISSOR)
+pg.display.set_icon(VICT)
 
-GAME = {'rock':'scissors',
+game = {'rock':'scissor',
         'paper': 'rock',
-        'scissors':'paper'}
+        'scissor':'paper'}
+
+counter = {y:x for x,y in game.items()}
+
+sprites = {'vict': Hand(VICT),
+           'lose': Hand(LOSE),
+           'rock': Hand(ROCK),
+           'paper': Hand(PAPER),
+           'scissor': Hand(SCISSOR)}
+
+buttons = [Button(ROCK, 1), Button(PAPER, 2), Button(SCISSOR, 3)]
+
 
 def main():
-    while True:
-        pg.draw.rect(WIN, (255,255,255), (0,0,WIDTH, HEIGHT))
-        WIN.blit(ROCK, (50, 50))
-        WIN.blit(LOSE, (500, 50))
-        pg.display.update()
+    clock = pg.time.Clock()
 
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                quit()
+    def main_menu():
+        return
+
+    def game():
+        pSelect = 'rock'
+        bSelect = 'rock'
+
+        def update():
+            WIN.fill((255,255,128))
+            sprites[pSelect].draw(WIN)
+            sprites[bSelect].draw(WIN, True)
+
+            for button in buttons:
+                button.draw(WIN)
+
+            pg.display.update()
+
+        while True:
+            clock.tick(30)
+            update()
+
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    quit()
+
+            if pg.mouse.get_pressed()[0]:
+                if buttons[0].is_pressed():
+                    pSelect = 'rock'
+                elif buttons[1].is_pressed():
+                    pSelect = 'paper'
+                elif buttons[2].is_pressed():
+                    pSelect = 'scissor'
+
+            bSelect = counter[pSelect]
+
+
+    def game_over():
+        return
+
+    while True:
+        main_menu()
+        game()
+        game_over()
 
 main()
